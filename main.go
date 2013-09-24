434a6288b8d00aaa5a2fb769956402a0e697e243
calmer <calmer91@gmail.com>
2013-09-24 20:34:03 +0800

    Remove test.go

--- main.go
+++ main.go
@@ -7,13 +7,12 @@ import (
 )
 
 func main() {
-	//runtime.GOMAXPROCS(runtime.NumCPU())
+	runtime.GOMAXPROCS(runtime.NumCPU())
 	fmt.Print("start=====\n")
-	taskManager := model.NewInstance(3)
+	taskManager := model.NewInstance(10)
 	//task := &model.PrintTask{Text: "hello"}
 	task := model.NewFetchTaskInstance(1, 10, "c:\\temp\\")
 	taskManager.AddTask(task)
-	fmt.Printf("end=====%v\n", taskManager)
 	taskManager.Run()
 	var str, str1 string
 	fmt.Scanf("%s%s", &str, &str1)
@@ -21,5 +20,4 @@ func main() {
 		taskManager.Stop()
 		fmt.Println("taskManager stop")
 	}
-	//fmt.Scanln(&str1)
 }
